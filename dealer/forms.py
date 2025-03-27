dde8776322d79922d2b1f86e9eb5207f159087af
Valeev Azamat <azamat@mail.sao.ru>
2025-03-28 02:46:07 +0300

    dealer app: *VersionFormset* - both Product+Version edit

--- dealer/forms.py
+++ dealer/forms.py
@@ -1,10 +1,20 @@
 from django import forms
+from django.forms import BooleanField
 
 from dealer.models import Product, Category, Version
 
 forbidden_words = ["бесплатно", "биржа", "биржам", "биржами", "биржах", "бирже", "биржей", "биржи", "биржу", "дешево", "дешевле", "казино", "крипта", "криптам", "криптами", "криптах", "крипте", "криптовалют", "криптовалюта", "криптовалютам", "криптовалютами", "криптовалютах", "криптовалюте", "криптовалютой", "криптовалюту", "криптовалюты", "криптой", "крипту", "крипты", "обман", "обмана", "обманам", "обманами", "обманах", "обмане", "обманов", "обманом", "обману", "обманы", "полицией", "полиции", "полиций", "полицию", "полиция", "полициям", "полициями", "полициях", "радар", "радара", "радарам", "радарами", "радарах", "радаре", "радаров", "радаром", "радару", "радары"]
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control text-start'
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     def clean_product_name(self):
         cleaned_data = self.cleaned_data['product_name']
         for word in forbidden_words:
@@ -18,12 +28,12 @@ class ProductForm(forms.ModelForm):
         model = Product  # Обязательно указываем модель
         exclude = ('views_count','slug')  # и перечисляем поля для отображения
 
-class CategoryForm(forms.ModelForm):
+class CategoryForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Category  # Обязательно указываем модель
         fields = '__all__'
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version  # Обязательно указываем модель
         fields = '__all__'
--- dealer/views.py
+++ dealer/views.py
@@ -1,12 +1,13 @@
 from itertools import product
 
+from django.forms.models import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
-from dealer.models import Product, Category
-from dealer.forms import ProductForm, CategoryForm
+from dealer.models import Product, Category, Version
+from dealer.forms import ProductForm, CategoryForm, VersionForm
 
 class ProductListView(ListView):
     model = Product
@@ -31,14 +32,30 @@ class ProductUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('dealer:detail', args=[self.kwargs.get('pk')] )
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+        return context_data
 
     def form_valid(self, form):
-        if form.is_valid():
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+
             new_item = form.save()
             new_item.slug = slugify(new_item.product_name)
             new_item.save()
-        return super().form_valid(form)
 
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
 class ProductDeleteView(DeleteView):
     model = Product
